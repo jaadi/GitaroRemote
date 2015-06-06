9ec9c49dcd7cadb4617cf9de41d1203add144c22
Jaadi <naimgoog@gmail.com>
2015-06-07 01:15:03 +0200

    first commit

---
+++ Gitaro/src/pac1/AA.java
@@ -0,0 +1,13 @@
+package pac1;
+
+public class AA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("very simple nur die main und syso");
+		System.out.println("other syso");
+	}
+
+}
