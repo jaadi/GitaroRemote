1712602ace1a47e60197ce61f7780f24d961560a
Jaadi <naimgoog@gmail.com>
2015-06-09 20:41:19 +0200

    new class created with getter and setter and constructor.

---
+++ Gitaro/src/pac1/BB.java
@@ -0,0 +1,24 @@
+package pac1;
+
+public class BB {
+	
+	
+	private String salutation ;
+	
+	public BB(String salutation){
+		
+		this.salutation = salutation;
+		
+	}
+
+	public String getSalutation() {
+		return salutation;
+	}
+
+	public void setSalutation(String salutation) {
+		this.salutation = salutation;
+	}
+	
+	
+
+}
